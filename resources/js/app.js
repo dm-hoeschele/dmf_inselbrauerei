922b836a52b8d0fc7f769a967f68004b226208a5
Carsten Höschele <carstenhoschele@MacBook-Pro-von-Carsten.local>
2021-07-07 12:14:19 +0200

    Implementierung des Shop-Submenüs und Anpassung des Footers

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,19 @@
 const hamburger = document.getElementById('hamburger');
 const menu_items = document.getElementById('menu-items');
+const menu_shop = document.getElementById('menu_shop');
+const shop_subcat = document.getElementById('shop_subcategories');
+
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('isactive');
     menu_items.classList.toggle('active');
     /* console.log('Menu clicked'); */
   });
+
+
+menu_shop.addEventListener('click', function() {
+    /* hamburger.classList.toggle('isactive'); */
+    shop_subcat.classList.toggle('active');
+})
 
 
